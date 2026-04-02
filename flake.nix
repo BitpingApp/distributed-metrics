34555dd9687b1330dba77ed16d91c9f4c0fd6958
Nick Carton <nick@firaenix.me>
2026-04-02 12:23:34 +0200

    Fix dependabot + add flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "distributed-metrics - Prometheus metrics backed by Bitping's distributed network";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    ...
+  }:
+  let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor =
+      system:
+      import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+  in
+  {
+    devShells = forAllSystems (
+      system:
+      let
+        pkgs = pkgsFor system;
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+          ];
+        };
+      in
+      {
+        default = pkgs.mkShell {
+          buildInputs =
+            [
+              rustToolchain
+              pkgs.cargo-audit
+              pkgs.cargo-watch
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+              pkgs.darwin.apple_sdk.frameworks.Security
+              pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
+
+          env = {
+            RUST_BACKTRACE = "1";
+          };
+        };
+      }
+    );
+  };
+}
