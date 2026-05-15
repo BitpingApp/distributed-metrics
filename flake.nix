bc322c5d5e33c60320547e6d7db42323484f7cfd
Nick Carton <nick@firaenix.me>
2026-05-15 11:09:44 +0200

    bump 1.3.0

--- flake.nix
+++ flake.nix
@@ -9,69 +9,70 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    rust-overlay,
-    ...
-  }:
-  let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor =
-      system:
-      import nixpkgs {
-        inherit system;
-        overlays = [ rust-overlay.overlays.default ];
-      };
-  in
-  {
-    packages = forAllSystems (
-      system:
-      let
-        pkgs = pkgsFor system;
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-      in
-      {
-        default = pkgs.rustPlatform.buildRustPackage {
-          pname = "distributed-metrics";
-          version = "1.3.0";
-          src = ./.;
-          cargoHash = "";
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
         };
-      }
-    );
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          rustToolchain = pkgs.rust-bin.stable.latest.default;
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "distributed-metrics";
+            version = "1.3.0";
+            src = ./.;
+            cargoHash = "sha256-d8ha7Epa+slx33SrZG0jre3oXZ5wYkHhzvMyX0TrElc=";
+          };
+        }
+      );
 
-    devShells = forAllSystems (
-      system:
-      let
-        pkgs = pkgsFor system;
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-            "clippy"
-          ];
-        };
-      in
-      {
-        default = pkgs.mkShell {
-          buildInputs = [
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
               rustToolchain
               pkgs.cargo-audit
               pkgs.cargo-watch
               pkgs.cargo-dist
             ];
 
-          env = {
-            RUST_BACKTRACE = "1";
+            env = {
+              RUST_BACKTRACE = "1";
+            };
           };
-        };
-      }
-    );
-  };
+        }
+      );
+    };
 }
